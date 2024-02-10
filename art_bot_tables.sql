da8c35ddc55651f498da3186cb9de8edc1043a1d
Limar Aryan <laryan@gmu.edu>
2024-02-10 03:07:49 -0500

    Started object table for title, year, and maybe author Need to finish adding variable types

--- art_bot_tables.sql
+++ art_bot_tables.sql
@@ -16,3 +16,35 @@ CREATE TABLE published_images (
 
 
 --Add objects table to get title, artist, and year
+CREATE TABLE object (
+    objectid INTEGER PRIMARY KEY,
+    accessioned INTEGER,
+    accessionnum VARCHAR(32),
+    locationid INTEGER,
+    title VARCHAR(2048),
+    displaydate VARCHAR(256),
+    beginyear INTEGER,
+    endyear INTEGER,
+    visualbrowsertimespan VARCHAR(32),
+    medium VARCHAR(2048),
+    dimensions VARCHAR(2048),
+    inscription VARCHAR,
+    markings VARCHAR,
+    attributioninverted --TODO: Add Variable Types
+    attribution
+    provenancetext
+    creditline
+    classification
+    subclassification
+    visualbrowserclassification
+    parentid
+    isvirtual
+    departmentabbr
+    portfolio
+    series
+    volume
+    watermarks
+    lastdetectedmodification
+    wikidataid
+    customprinturl
+);
